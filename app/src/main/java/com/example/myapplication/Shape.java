cfbd8c83ddaecd92a57ba268a24ba0ebb5fac043
damansin <damanbatra15@gmail.com>
2024-09-27 03:56:31 -0600

    Resolved merge conflict

--- app/src/main/java/com/example/myapplication/Shape.java
+++ app/src/main/java/com/example/myapplication/Shape.java
@@ -3,7 +3,7 @@ package com.example.myapplication;
 public abstract class Shape {
     int x;
     int y;
-    String color = "Pink";
+    String color = "Red";
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
