3c23e408e08ec8bedd77c33035ac2c7c9d8bbe2e
David Gong <60947042+disloyalfire@users.noreply.github.com>
2024-09-26 22:07:05 -0600

    Update Shape.java

--- app/src/main/java/com/example/myapplication/Shape.java
+++ app/src/main/java/com/example/myapplication/Shape.java
@@ -3,7 +3,7 @@ package com.example.myapplication;
 public abstract class Shape {
     int x;
     int y;
-    String colour = "Yellow";
+    String colour = "Purple";
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
