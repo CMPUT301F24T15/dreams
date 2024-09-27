1214e3e271398b71f905adb6289a11b396c668ff
Ahmed <ashittu38@gmail.com>
2024-09-26 20:14:50 -0600

    Changed color variable in main

--- app/src/main/java/com/example/myapplication/Shape.java
+++ app/src/main/java/com/example/myapplication/Shape.java
@@ -3,7 +3,7 @@ package com.example.myapplication;
 public abstract class Shape {
     int x;
     int y;
-    String colour = "Yellow";
+    String colour = "Magenta";
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
